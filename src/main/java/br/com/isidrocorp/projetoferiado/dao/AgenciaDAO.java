d7d9398d35da946fc80c78878f04aa0dcf92bfe5
leoswats <leoswats@gmail.com>
2020-10-16 14:48:26 -0300

    delete estrutura isidro

--- src/main/java/br/com/isidrocorp/projetoferiado/dao/AgenciaDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.isidrocorp.projetoferiado.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.isidrocorp.projetoferiado.model.Agencia;
-
-public interface AgenciaDAO extends CrudRepository<Agencia,Integer>{
-
-}
